7cdf41cfe0cada6161f3175e3eb21c2e69157739
Lucas Brito <lucasbfmatos@hotmail.com>
2021-11-01 01:01:36 -0300

    added infinite scrolling

--- src/pages/[login].tsx
+++ src/pages/[login].tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
-import { gql, useQuery } from "@apollo/client";
+import { gql, useApolloClient, useQuery } from "@apollo/client";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { cloneDeep } from "lodash";
 
 import styles from "../styles/pages/User.module.css";
 import { Brand } from "../components/Brand";
@@ -15,6 +17,7 @@ import { Icon } from "../components/Icon";
 const GET_USER = gql`
   query UserQuery($login: String!) {
     user(login: $login) {
+      id
       name
       avatarUrl
       location
@@ -29,10 +32,14 @@ const GET_USER = gql`
           name
         }
       }
-      repositories(first: 20) {
+      repositories(first: 6) {
         nodes {
           id
           ...RepositoryItemFields
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
         }
         totalCount
       }
@@ -41,10 +48,30 @@ const GET_USER = gql`
   ${REPOSITORY_ITEM_FRAGMENT}
 `;
 
+const GET_REPOSITORIES = gql`
+  query RepositoriesQuery($login: String!, $cursor: String) {
+    user(login: $login) {
+      id
+      repositories(first: 6, after: $cursor) {
+        nodes {
+          id
+          ...RepositoryItemFields
+        }
+        pageInfo {
+          endCursor
+          hasNextPage
+        }
+      }
+    }
+  }
+  ${REPOSITORY_ITEM_FRAGMENT}
+`;
+
 function User() {
   const router = useRouter();
+  const client = useApolloClient();
   const [hasError, setHasError] = useState(false);
-  const { loading, error, data } = useQuery(GET_USER, {
+  const { loading, error, data, updateQuery } = useQuery(GET_USER, {
     variables: { login: router.query.login },
   });
 
@@ -63,6 +90,31 @@ function User() {
     router.push("/" + target.search.value);
   }
 
+  async function fetchMore() {
+    if (!data || !data.user.repositories.pageInfo.hasNextPage) return;
+
+    const res = await client.query({
+      query: GET_REPOSITORIES,
+      variables: {
+        login: router.query.login,
+        cursor: data.user.repositories.pageInfo.endCursor,
+      },
+    });
+
+    await updateQuery((previousResult) => {
+      const nextResult = cloneDeep(previousResult);
+      const nextRepositories = nextResult.user.repositories;
+
+      nextRepositories.nodes = [
+        ...nextRepositories.nodes,
+        ...res.data.user.repositories.nodes,
+      ];
+      nextRepositories.pageInfo = res.data.user.repositories.pageInfo;
+
+      return nextResult;
+    });
+  }
+
   const userNotFound = !loading && error;
 
   let content;
@@ -72,12 +124,23 @@ function User() {
   } else if (loading) {
     content = <div>Loading...</div>;
   } else {
-    content = data.user.repositories.nodes.map(
-      (
-        repo: RepositoryItemProps & {
-          id: string;
-        }
-      ) => <RepositoryItem key={repo.id} {...repo} />
+    content = (
+      <InfiniteScroll
+        dataLength={data.user.repositories.nodes.length}
+        next={fetchMore}
+        hasMore={data.user.repositories.pageInfo.hasNextPage}
+        loader={<div>Loading...</div>}
+      >
+        {data.user.repositories.nodes.map(
+          (
+            repo: RepositoryItemProps & {
+              id: string;
+            }
+          ) => (
+            <RepositoryItem key={repo.id} {...repo} />
+          )
+        )}
+      </InfiniteScroll>
     );
   }
 
